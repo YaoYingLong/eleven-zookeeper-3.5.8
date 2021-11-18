3b4c0d50d2f13a646bdc247e35c6f4fe6370039d
Y31201308 <906271196@qq.com>
2021-11-18 20:56:39 +0800

    init project

---
+++ zookeeper-server/src/main/java/org/apache/zookeeper/Info.java
@@ -0,0 +1,14 @@
+package org.apache.zookeeper;
+
+/**
+ * @author by YingLong on 2021/11/18
+ */
+public interface Info {
+    int MAJOR = 1;
+    int MINOR = 0;
+    int MICRO = 0;
+    String QUALIFIER = null;
+    int REVISION = -1;
+    String REVISION_HASH = "1";
+    String BUILD_DATE = "2020-10-15";
+}
